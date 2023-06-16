178cf681954de6b175c4fe658d600c72080d8672
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-06-16 18:35:12 +0200

    Done with Section 2 JS Refresher

--- 02-javascript-refresher/assets/scripts/app.js
+++ 02-javascript-refresher/assets/scripts/app.js
@@ -7,4 +7,19 @@ console.log(apiKey + " " + abc);
 // Could also import all into one JS object by doing:
 // import * as util from "./util.js";
 
+// We could then access everything using console.log(util.)
+// Here util. will display all available imports after we write .
+// That way we could have for example console.log(util.apiKey)
+
 // Do bear in mind we can have default imports and exports and normal ones at the same time
+
+// Coding exercise 2 React Course Udemy
+function transformToObjects(numberArray) {
+    // Todo: Add your logic
+    // should return an array of objects
+    const transform = numberArray.map((item) => ({val : item}));
+    return transform;
+}
+
+const a = [1, 2, 3]
+console.log(transformToObjects(a))
