8053d52c06597d836f5f9c0c9b6a4aba91af80fd
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-07-10 18:29:45 +0200

    Added comments and tweaked the JSX code a bit

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,14 @@
+// A React compoenent that is exported into index.js main root file where the react library is rendered
+// HTML code in this javascript code is called JSX (javascript XML), this is valid syntax because
+// NPM start transforms all this code into valid code for the web browser.
+// By right clicking on inspect webpage and going to debugger and checking out the bundle.js
+// subfile, we can see the React code that happens "under the hood" a.k.a the transformed code.
 function App() {
+  // the HTML code below is our desired target state, which is what we want REACT to render
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p> Testing testing 1 2 3</p>
     </div>
   );
 }
--- 01-starting-setup/src/index.js
+++ 01-starting-setup/src/index.js
@@ -1,7 +1,14 @@
+// This imports a ReactDOM object from the package.json file and this object provides
+// all the methods needed to render React components in the browser.
 import ReactDOM from 'react-dom/client';
-
+// Imports all the components of index.css into this index.js file
 import './index.css';
+// Imports all the logic from the app.js file into this one
 import App from './App';
 
+// This is the file that starts everything in React
+// We start our React application in the index.html file's "root" div
+// The content of the "root" div in index.html is replaced with the app.js logic
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// App is a component and the render method renders the app component into the root object which we stored in const root.
 root.render(<App />);
