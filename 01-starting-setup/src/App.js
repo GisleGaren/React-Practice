54f0884c02e8e7e5745cda4a6f004d29e63215ca
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-07-12 18:50:49 +0200

    Added som CSS and dynamic data

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -3,12 +3,17 @@
 // NPM start transforms all this code into valid code for the web browser.
 // By right clicking on inspect webpage and going to debugger and checking out the bundle.js
 // subfile, we can see the React code that happens "under the hood" a.k.a the transformed code.
+
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   // the HTML code below is our desired target state, which is what we want REACT to render
+  // Convention is that lower case elements are built in elements, whilst uppercase are custom made elements
+  // We use the name of the import dom object when we want to implement these custom elements
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p> Testing testing 1 2 3</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,1 +1,23 @@
 // With React components, it is typical to name the files with a capital letter
+
+import "./ExpenseItem.css"
+
+// It is very important to not use "class" in each html element, but instead className
+// because it is common convention to do so in React.
+// toISOString is a toString method that we use to print out a format for the Date object
+function ExpenseItem() {
+    const expenseDate = new Date(2023, 1, 1);
+    const expenseTitle = "Bil Forsikring";
+    const expenseAmount = 294.19;
+  return (
+    <div className="expense-item">
+      <div> {expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2> {expenseTitle} </h2>
+        <div className="expense-item__price"> {expenseAmount} kr</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
