47d318570782337abf58ee8a5f6862c073d6f622
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-07-27 16:06:23 +0200

    Added a calender into apps.js

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -8,10 +8,19 @@ import "./ExpenseItem.css"
 
 // props allow React components to become reusable by allowing us to exchange data from a parent container to its children
 // instead of hardcoding everything
+// We could also use "object destructuring" by writing ({date, title, amount}) instead of (props)
+// in order to exchange data between components. You would also need to remove props.date to just date
 function ExpenseItem(props) {
-
+  const month = props.date.toLocaleString('en-US', { month: 'long'});
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US', { day: '2-digit'});
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div> {props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2> {props.title} </h2>
