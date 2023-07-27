ae6cb87eedccbf92b6e8366ca86f22086ec85ec5
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-07-27 13:00:41 +0200

    Trying to understand props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -5,16 +5,17 @@ import "./ExpenseItem.css"
 // It is very important to not use "class" in each html element, but instead className
 // because it is common convention to do so in React.
 // toISOString is a toString method that we use to print out a format for the Date object
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 1, 1);
-    const expenseTitle = "Bil Forsikring";
-    const expenseAmount = 294.19;
+
+// props allow React components to become reusable by allowing us to exchange data from a parent container to its children
+// instead of hardcoding everything
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div> {expenseDate.toISOString()}</div>
+      <div> {props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2> {expenseTitle} </h2>
-        <div className="expense-item__price"> {expenseAmount} kr</div>
+        <h2> {props.title} </h2>
+        <div className="expense-item__price"> {props.amount} kr</div>
       </div>
     </div>
   );
