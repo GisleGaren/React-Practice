54cf9754cec847915a3b350ce276f872b19c32aa
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-08-25 16:42:33 +0200

    Intro to Styled Components library

--- 04-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 04-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
-
+import { styled } from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  & invalid input {
+    border-color:red;
+    background:rosybrown;
+  }
+
+  & invalid label{
+    color: red;
+  }
+`;
 
 const CourseInput = props => {
   // Initially the entered value in the input box is empty
@@ -25,17 +58,14 @@ const CourseInput = props => {
     }
     props.onAddGoal(enteredValue);
   };
-
+  // the `` are backticks default in javascript or known as a template literal
+  // <div className={`form-control ${!isValid ? "invalid" : ""}`}>
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input type="text" style={{
-          borderColor: !isValid ? "red" : "black",
-          backgroundColor: !isValid ? "salmon" : "transparent"
-          }}
-          onChange={goalInputChangeHandler} />
-      </div>
+      <FormControl>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
